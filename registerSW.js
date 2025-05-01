5db1efaa55957e3bf433d1c8245a743e43af904e
Wijsbe <wijsbe@github.com>
2025-05-01 18:03:50 +0200

    Update: Remove auto-change of dienst on calendar click

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/daily-scedule/sw.js', { scope: '/daily-scedule/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
